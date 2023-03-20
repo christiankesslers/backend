19d71b8367eea5d6c772ee901038b49fb928c1a8
Roy Hodge Jr <118377684+modelomaker812@users.noreply.github.com>
2023-03-20 12:07:47 -0400

    Update index.js
    added dummy endpoints to messagePage in db

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,11 +6,28 @@ import { userPage } from '../controllers/user';
 
 const indexRouter = express.Router();
 indexRouter.get('/', indexPage);
+// messages endpoint
 indexRouter.get('/messages', messagesPage);
 indexRouter.post('/messages', modifyMessage, performAsyncAction, addMessage);
+// user endpoint
 indexRouter.get('/user', userPage);
 indexRouter.post('/user', modifyMessage, performAsyncAction, addMessage);
 
+// auth/register post endpoint
+indexRouter.post('/auth/register', modifyMessage, performAsyncAction, addMessage);
+
+// auth/login post endpoint
+indexRouter.post('/auth/login', modifyMessage, performAsyncAction, addMessage);
+
+// auth/logout post endpoint
+indexRouter.post('/auth/logout', modifyMessage, performAsyncAction, addMessage);
+
+// auth/forgot-password post endpoint
+indexRouter.post('/auth/forgot-password', modifyMessage, performAsyncAction, addMessage);
+
+// /tasks get endpoint
+indexRouter.get('/tasks', messagesPage);
+indexRouter.post('/tasks', modifyMessage, performAsyncAction, addMessage);
 
 
 export default indexRouter;
